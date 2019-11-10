527d20402c393f08a332ff308df5587f69cf5285
krzysztof.paszkowski@intive.com <krzysztof.paszkowski@intive.com>
2019-11-10 16:48:47 +0100

    complete oauth

--- ServerWepApi/Startup.cs
+++ ServerWepApi/Startup.cs
@@ -1,4 +1,8 @@
-﻿using Owin;
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using ServerWepApi.Providers;
+using System;
 using System.Web.Http;
 
 namespace ServerWepApi
@@ -8,8 +12,28 @@ namespace ServerWepApi
         public void Configuration(IAppBuilder app)
         {
             HttpConfiguration config = new HttpConfiguration();
+
+            ConfigureOAuth(app);
+
             WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
         }
     }
 }
