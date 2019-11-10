95f795dc5d7821de559612b321f0d50ecf28924d
krzysztof.paszkowski@intive.com <krzysztof.paszkowski@intive.com>
2019-11-10 15:58:27 +0100

    simple orders controller

--- ServerWepApi/Controllers/OrdersController.cs
+++ ServerWepApi/Controllers/OrdersController.cs
@@ -1,17 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Http;
 
 namespace ServerWepApi.Controllers
 {
-    public class OrdersController : Controller
+    [RoutePrefix("api/Orders")]
+    public class OrdersController : ApiController
     {
-        // GET: Orders
-        public ActionResult Index()
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
         {
-            return View();
+            return Ok(Order.CreateOrders());
+        }
+
+    }
+
+    #region Helpers
+
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public string CustomerName { get; set; }
+        public string ShipperCity { get; set; }
+        public bool IsShipped { get; set; }
+
+        public static List<Order> CreateOrders()
+        {
+            List<Order> OrderList = new List<Order>
+            {
+                new Order {OrderID = 10248, CustomerName = "Taiseer Joudeh", ShipperCity = "Amman", IsShipped = true },
+                new Order {OrderID = 10249, CustomerName = "Ahmad Hasan", ShipperCity = "Dubai", IsShipped = false},
+                new Order {OrderID = 10250,CustomerName = "Tamer Yaser", ShipperCity = "Jeddah", IsShipped = false },
+                new Order {OrderID = 10251,CustomerName = "Lina Majed", ShipperCity = "Abu Dhabi", IsShipped = false},
+                new Order {OrderID = 10252,CustomerName = "Yasmeen Rami", ShipperCity = "Kuwait", IsShipped = true}
+            };
+
+            return OrderList;
         }
     }
+
+    #endregion
 }
